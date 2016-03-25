c8b482f5382fa60e76cbc82c5362b97145d35d9a
Valentino Hudhra <v.hudhra@gmail.com>
2016-03-25 13:32:58 +0000

    Commiting hacks to help the server.

--- ckanext/qa/commands.py
+++ ckanext/qa/commands.py
@@ -104,7 +104,7 @@ class QACommand(p.toolkit.CkanCommand):
         from ckanext.qa import plugin
         packages = []
         resources = []
-        if len(self.args) > 1:
+        if len(self.args) > 1 and (self.args[1] != 'first' and self.args[1] != 'second'):
             for arg in self.args[1:]:
                 # try arg as a group id/name
                 group = model.Group.get(arg)
@@ -135,8 +135,15 @@ class QACommand(p.toolkit.CkanCommand):
             # all packages
             pkgs = model.Session.query(model.Package)\
                         .filter_by(state='active')\
+                        .filter_by(type='dataset')\
                         .order_by('name').all()
             packages.extend(pkgs)
+            # hack to ensure that our server does not die
+            if len(self.args) > 1:
+                if self.args[1] == 'first':
+                    packages = packages[:len(packages)/2]
+                elif self.args[1] == 'second':
+                    packages = packages[len(packages)/2:]
             if not self.options.queue:
                 self.options.queue = 'bulk'
 
